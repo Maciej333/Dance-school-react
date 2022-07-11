d379e404367c5f7ef5678ce121244bbdf1a48949
Maciej333 <maciej.rogowski333@gmail.com>
2022-07-11 18:58:44 +0200

    add footer icons

--- src/features/Home/Footer/FooterAbout/FooterAbout.tsx
+++ src/features/Home/Footer/FooterAbout/FooterAbout.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './FooterAbout.style.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserGroup } from '@fortawesome/free-solid-svg-icons';
 
 export default function FooterAbout() {
     return (
         <div className='footer-section footer-about'>
             <h3>About US</h3>
             <p>We are the group of people who love to share their passion with others! If You want to experience an amazing adventure just join us!</p>
-            <NavLink to="/group"><span>ICON</span>Our groups!</NavLink>
+            <NavLink to="/group"><FontAwesomeIcon icon={faUserGroup} />Our groups!</NavLink>
         </div>
     )
 }
--- src/features/Home/Footer/FooterContact/FooterContact.tsx
+++ src/features/Home/Footer/FooterContact/FooterContact.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './FooterContact.style.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 export default function FooterContact() {
     return (
         <div className='footer-section footer-contact'>
             <h3>Contact</h3>
-            <a href="tel:+XXX-ZZZ-YYY" style={{ marginTop: '0px' }}><span>ICON</span>111-222-ZZZ</a>
-            <a href="mailto: abc@danceXYZ.zzz"><span>ICON</span>dance.school@dance.com</a>
-            <a href="https://pl-pl.facebook.com/"><span>ICON</span>facebook</a>
-            <a href="https://www.instagram.com/"><span>ICON</span>instagram</a>
+            <a href="tel:+XXX-ZZZ-YYY" style={{ marginTop: '0px' }}><FontAwesomeIcon icon={faPhone} />111-222-ZZZ</a>
+            <a href="mailto: abc@danceXYZ.zzz"><FontAwesomeIcon icon={faEnvelope} />dance.school@dance.com</a>
+            <a href="https://pl-pl.facebook.com/"><FontAwesomeIcon icon={faFacebook} />facebook</a>
+            <a href="https://www.instagram.com/"><FontAwesomeIcon icon={faInstagram} />instagram</a>
         </div>
     )
 }
