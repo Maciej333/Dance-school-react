f919b6671b040b569e46e68e3ac0fad8b1df6f1c
Maciej333 <maciej.rogowski333@gmail.com>
2022-07-15 14:09:53 +0200

    add single element return button

--- src/app/components/SingleElement/SingleElement.tsx
+++ src/app/components/SingleElement/SingleElement.tsx
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
 import './SingleElement.style.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLeftLong } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 export default function SingleElement(props: { Operations: React.ComponentType | null, children: JSX.Element | null }) {
 
     const { Operations, children } = props;
-
+    const navigate = useNavigate();
     const [show, setShow] = useState(false);
 
+    const handleReturn = () => {
+        navigate(-1);
+    }
     return (
         <div className='single-element'>
             {
                 Operations ?
                     <div className='operations'>
-                        <button className='btn operations-btn' onClick={() => setShow(prev => !prev)}>Operations</button>
-                        <div className={`operations-list ${show ? "show" : ""}`}>
-                            <Operations />
+                        <FontAwesomeIcon icon={faLeftLong} className="return-btn" onClick={handleReturn} />
+                        <div className='operations-btn-wrapper'>
+                            <button className='btn operations-btn' onClick={() => setShow(prev => !prev)}>Operations</button>
+                            <div className={`operations-list ${show ? "show" : ""}`}>
+                                <Operations />
+                            </div>
                         </div>
                     </div>
                     :
