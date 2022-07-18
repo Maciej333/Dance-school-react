7e9c937f040b9fddfe7207fda401b7d084374643
Maciej333 <maciej.rogowski333@gmail.com>
2022-07-18 23:21:03 +0200

    change route to protected route for group forms

--- src/features/Group/routes/GroupRoutes.tsx
+++ src/features/Group/routes/GroupRoutes.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Outlet, Route, Routes } from 'react-router-dom';
 import ReturnHeader from '../../../app/components/ReturnHeader/ReturnHeader';
+import ProtectedRoute from '../../../app/routes/ProtectedRoute/ProtectedRoute';
+import { UserRole } from '../../../app/utils/enum/UserRole.enum';
 import GroupForm, { FORM_ADD, FORM_EDIT } from '../GroupForm/GroupForm';
 import MultiGroups from '../MultiGroups/MultiGroups';
 import SingleGroup from '../SingleGroup/SingleGroup';
@@ -10,8 +12,28 @@ export default function GroupRoutes() {
         <Routes>
             <Route path="" element={<Outlet />}>
                 <Route path="/" element={<MultiGroups />} />
-                <Route path="add" element={<><ReturnHeader toNavigate="/group" name="Add group" /><GroupForm type={FORM_ADD} /></>} />
-                <Route path="edit/:id" element={<><ReturnHeader toNavigate="/group" withProp={true} name="Edit group" /><GroupForm type={FORM_EDIT} /></>} />
+                <Route
+                    path="add"
+                    element={
+                        <ProtectedRoute auths={[UserRole.INSTRUCTOR]}>
+                            <>
+                                <ReturnHeader toNavigate="/group" name="Add group" />
+                                <GroupForm type={FORM_ADD} />
+                            </>
+                        </ProtectedRoute>
+                    }
+                />
+                <Route
+                    path="edit/:id"
+                    element={
+                        <ProtectedRoute auths={[UserRole.DIRECTOR, UserRole.INSTRUCTOR]}>
+                            <>
+                                <ReturnHeader toNavigate="/group" withProp={true} name="Edit group" />
+                                <GroupForm type={FORM_EDIT} />
+                            </>
+                        </ProtectedRoute>
+                    }
+                />
                 <Route path=":id" element={<SingleGroup />} />
             </Route>
 
